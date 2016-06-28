bba119a60da11ae06f417574af55983ea93baa0c
samyak jain <samyak jain>
2016-06-28 08:51:50 +0530

    A-1 assignment html

---
+++ assignment1/js/JSON.js
@@ -0,0 +1,11 @@
+/**
+ * Created by smyk0 on 6/27/2016.
+ */
+var samyak = {
+
+    "firstName" : "Samyak",
+        "lastName" : "Jain",
+        "salary" : 350000 ,
+        "dob" : new Date(1993,11,21),
+        "email" : "smyk009@gmail.com"
+};
